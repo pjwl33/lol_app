799ffb2c5abaeed5a9f18fd921a2c874fd12613b
pjwl33 <paul.leejw@gmail.com>
2014-09-26 01:26:36 -0400

    heroku

--- app/assets/javascripts/angular-app/routes/index.js
+++ app/assets/javascripts/angular-app/routes/index.js
@@ -2,19 +2,19 @@ ang.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
     when('/', {
-      templateUrl: '/ang-views/index.html',
+      templateUrl: '/assets/javascripts/angular-app/views/index.html',
       controller: 'HomeCtrl'
     }).
     when('/group_dynamics', {
-      templateUrl: '/ang-views/group_dynamics.html',
+      templateUrl: '/assets/javascripts/angular-app/views/group_dynamics.html',
       controller: 'HomeCtrl'
     }).
     when('/comfort_picks', {
-      templateUrl: '/ang-views/comfort_picks.html',
+      templateUrl: '/assets/javascripts/angular-app/views/comfort_picks.html',
       controller: 'HomeCtrl'
     }).
     when('/team_compositions', {
-      templateUrl: '/ang-views/team_compositions.html',
+      templateUrl: '/assets/javascripts/angular-app/views/team_compositions.html',
       controller: 'HomeCtrl'
     }).
     when('/404', {
